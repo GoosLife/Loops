6251d65f13b1ab33e88990ff4d7581e1d32c1950
Frederik Hvid Sørensen <fred09j3@zbc.dk>
2022-03-22 09:44:34 +0100

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Exercise A
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        // Exercise B
+        for (int i = 0; i < 100; i++)
+        {
+            if (i < 50)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        // Exercise C
+        int iterator = 0;
+        while (iterator < 100)
+        {
+            Console.WriteLine(iterator);
+            iterator++;
+        }
+
+        // Exercise D
+        for (int i = 100; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
